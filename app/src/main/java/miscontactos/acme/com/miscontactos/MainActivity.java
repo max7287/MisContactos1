5cb327b43bced139496eabc7f16c940057d6a275
Luis Maximo <max7287@gmail.com>
2014-11-10 17:19:03 -0700

    Pantalla inicial

--- app/src/main/java/miscontactos/acme/com/miscontactos/MainActivity.java
+++ app/src/main/java/miscontactos/acme/com/miscontactos/MainActivity.java
@@ -2,8 +2,10 @@ package miscontactos.acme.com.miscontactos;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import miscontactos.acme.com.miscontactos.util.TextChangedListener;
 
@@ -11,6 +13,7 @@ import miscontactos.acme.com.miscontactos.util.TextChangedListener;
 public class MainActivity extends Activity {
 
     private EditText txtNombre,txtTelefono,txtEmail, txtDireccion;
+    private Button btnAgregar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +31,26 @@ public class MainActivity extends Activity {
         txtNombre.addTextChangedListener(new TextChangedListener(){
             @Override
             public void onTextChanged(CharSequence seq, int start, int before, int count) {
-                Button btnAgregar = (Button) findViewById(R.id.btnAgregar);
+                btnAgregar = (Button) findViewById(R.id.btnAgregar);
                 btnAgregar.setEnabled(!seq.toString().trim().isEmpty());
             }
         });
+    }
+
+    public void onClick(View view) {
+
+        String mesg = String.format("%s ha sido agregado a la lista!", txtNombre.getText());
+        Toast.makeText(this, mesg, Toast.LENGTH_SHORT).show();
+        btnAgregar.setEnabled(false);
+        limpiarCampos();
+    }
+
+    private void limpiarCampos() {
+        txtNombre.getText().clear();
+        txtTelefono.getText().clear();
+        txtEmail.getText().clear();
+        txtDireccion.getText().clear();
+        txtNombre.requestFocus();
+
     }
 }
